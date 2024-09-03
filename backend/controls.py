158ad8fbfd7406524528c4df39b9c5188a861dfc
Sameer <117622574+SameerShah666@users.noreply.github.com>
2024-09-04 00:13:09 +0530

    Add files via upload

--- backend/controls.py
+++ backend/controls.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 from flask import current_app as app
 from .models import *
-
-import json as j
+import datetime
 import hashlib as hash
 
+def date_pack(item):
+    date = datetime.datetime.strptime(item,'%Y-%m-%d')
+    return date
 
+def campaign_vis_pack(item):
+    visibility = {
+        '1':'Public',
+        '2':'Private'
+    }
+    return visibility.get(item)
+
+def inf_reach_pack(item):
+    r = str(item)
+    digits = len(r)
+    if digits > 6:
+        return r[:-6] + 'M'
+    elif digits < 4:
+        return r
+    else:
+        return r[:-3] + 'K'
+    
 def inf_platforms_pack(item):
     platforms = {
         "1":"Youtube",
@@ -17,7 +36,6 @@ def inf_platforms_pack(item):
     }
     packed_platforms = [platforms.get(i) for i in item]
     return ";".join(packed_platforms)
-
 
 def inf_niche_pack(item):
     niche_types = {
@@ -55,11 +73,9 @@ def validate_empty(item):
 def sha_256(item):
     return hash.sha256(item.encode('utf-8')).hexdigest()
 
-
 @app.route("/")
 def home():
     if 'user_id' not in session:
-        print("here is the issue")
         return redirect(url_for('login'))
     
     if session["user_id"][0] == "i":
@@ -68,12 +84,10 @@ def home():
     if session["user_id"][0] == "s":
         return redirect(url_for("spn_dashboard"))
     
+    if session["user_id"][0] == "a":
+        return redirect(url_for("adm_dashboard"))
+    
     return redirect(url_for('login'))
-
-@app.route("/trial", methods=['POST'])
-def trial():
-    data = request.get_json()
-    return {"result":data["value"]}
 
 @app.route("/login")
 def login():
@@ -104,7 +118,17 @@ def post_login():
     session["user_id"] = user.user_id
     session["user_name"] = user.user_name    
 
-    return render_template("login.html")
+    return redirect(url_for('home'))
+
+@app.route("/logout", methods=['POST','GET'])
+def Logout():
+    print("here is the issue")
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    
+    session.clear()
+    print("here")
+    return redirect(url_for('home'))
 
 @app.route("/register/influencer")
 def inf_register():
@@ -176,29 +200,310 @@ def post_spn_register():
     session["user_name"] = user.user_name
 
     print(spn_name, spn_email, spn_industry, spn_pass1, spn_pass2)
-    return render_template("sponsorregistration.html")
-
-@app.route("/dashboard/sponsor")
-def spn_dashboard():
-    if 'user_id' not in session:
-        print("here is the issue")
-        return redirect(url_for('login'))
-    return render_template('admindashboard')
+    return redirect(url_for("spn_dashboard"))
 
 @app.route("/dashboard/influencer")
 def inf_dashboard():
-    if 'user_id' not in session:
-        print("here is the issue")
-        return redirect(url_for('login'))
+    if 'user_id' not in session or session['user_id'][0] != 'i':
+        return redirect(url_for('home'))
     
     inf_id = session["user_id"]
     influencer = Influencer.query.filter_by(influencer_id=inf_id).first()
 
-    return render_template('Infuencerdashboard.html',name=influencer.influencer_name,email=influencer.influencer_email)
+    flash("You have logged in " + str(influencer.influencer_name))
+
+    print(influencer.adverts)
+
+    return render_template('Infuencerdashboard.html', nav=True, inf=influencer)
+
+@app.route("/dashboard/sponsor")
+def spn_dashboard():
+    if 'user_id' not in session or session['user_id'][0] != 's':
+        return redirect(url_for('home'))
+    sponsor = Sponsor.query.filter_by(sponsor_id=session.get('user_id')).first()
+    infDet = db.session.query(Influencer.influencer_id,Influencer.influencer_name).all()
+    
+    return render_template('sponsordashboard.html', nav = True, spn = sponsor, influencerdetails = infDet)
 
 @app.route("/dashboard/admin")
 def adm_dashboard():
-    if 'user_id' not in session:
-        print("here is the issue")
-        return redirect(url_for('login'))
+    if 'user_id' not in session or session['user_id'][0] != 'a':
+        return redirect(url_for('home'))
     return render_template('admindashboard.html')
+
+@app.route("/profile/influencer")
+def inf_profile():
+    if 'user_id' not in session or session['user_id'][0] != 'i':
+        return redirect(url_for('home'))
+    
+    flash("welcome to profile")
+    influencer = Influencer.query.filter_by(influencer_id=session.get('user_id')).first()
+
+    return render_template('influencerprofile.html',nav=True,inf=influencer)
+
+@app.route("/profile/influencer", methods=['POST'])
+def inf_profile_post():
+    if 'user_id' not in session or session['user_id'][0] != 'i':
+        return redirect(url_for('home'))
+    inf_name = validate_empty(request.form.get("infName"))
+    inf_niche = validate_empty(request.form.get("infNiche"))
+    inf_platforms = validate_empty(request.form.getlist("infPlatform"))
+    inf_reach = validate_empty(request.form.get("infReach"))
+    inf_pic = request.files['profile_pic_input']
+
+    print("here no shit:",inf_pic)
+
+    if None in [inf_name,inf_niche,inf_platforms,inf_reach]:
+        flash('Please Fill all the values')
+        return redirect(url_for('spn_profile'))
+
+    influencer = Influencer.query.filter_by(influencer_id=session.get('user_id')).first()
+    influencer.influencer_name = inf_name
+    influencer.influencer_niche = inf_niche_pack(inf_niche)
+    influencer.influencer_platforms = inf_platforms_pack(inf_platforms)
+    influencer.influencer_platform_reach = inf_reach_pack(inf_reach)
+    influencer.influencer_image = inf_pic.read()
+    
+    db.session.commit()
+
+    flash("Profile updated successfully")
+
+    return redirect(url_for('home'))
+
+@app.route("/profile/sponsor")
+def spn_profile():
+    if 'user_id' not in session or session['user_id'][0] != 'i':
+        return redirect(url_for('home'))
+    
+    flash("welcome to profile")
+
+    return render_template('sponsorprofile.html',nav=True)
+
+@app.route("/profile/sponsor", methods=['POST'])
+def spn_profile_post():
+    if 'user_id' not in session or session['user_id'][0] != 'i':
+        return redirect(url_for('home'))
+    spn_name = validate_empty(request.form.get("orgName"))
+    spn_indus = validate_empty(request.form.get("orgIndustry"))
+    spn_budget = validate_empty(request.form.get("orgBudget"))
+    spn_pic = request.files['profile_pic_input']
+
+    if None in [spn_name,spn_indus,spn_budget,spn_pic]:
+        flash('Please Fill all the values')
+        return redirect(url_for('spn_profile'))
+    
+    sponsor = Sponsor.query.filter_by(sponsor_id=session.get('user_id')).first()
+
+    sponsor.sponsor_name = spn_name
+    sponsor.sponsor_industry = spn_industry_pack(spn_indus)
+    sponsor.sponsor_budget = spn_budget
+    sponsor.sponsor_image = spn_pic.read()
+
+    print(sponsor.sponsor_name,sponsor.sponsor_industry,sponsor.sponsor_budget,sponsor.sponsor_image)
+
+    db.session.commit()
+
+    flash("Profile updated successfully")
+
+    return redirect(url_for('home'))
+
+@app.route("/search", methods=['POST'])
+def search():
+    if 'user_id' not in session:
+        return redirect(url_for('home'))
+    
+    item = request.form.get('searchItem')
+    if session.get('user_id')[0] == 'i':
+        campaign = Campaign.query.filter(
+            Campaign.campaign_name.contains(item)
+            ).all() + Campaign.query.filter(
+                Campaign.campaign_description.contains(item)
+                ).all()
+
+        return render_template('searchedcamp.html',nav=True, camps = set(campaign), item = item)
+    if session.get('user_id')[0] == 's':
+        inf = Influencer.query.filter(
+            Influencer.influencer_name.contains(item)
+        ).all() + Influencer.query.filter(
+            Influencer.influencer_niche.contains(item)
+        ).all() + Influencer.query.filter(
+            Influencer.influencer_platform_reach.contains(inf_reach_pack(item))
+        ).all()
+        
+        return render_template('searchedinfl.html', nav=True, infs = set(inf), item = item)
+
+@app.route("/create/campaign", methods=['POST'])
+def create_campaign():
+
+    if 'user_id' not in session or session['user_id'][0] != 's':
+        return redirect(url_for('home'))
+    
+    campaign_nam = request.form.get('campName')
+    campaign_vis = campaign_vis_pack(request.form.get('campVis'))
+    campaign_budg = request.form.get('campBudg')
+    campaign_desc = request.form.get('campDesc')
+    campaign_s_date = request.form.get('startDate')
+    campaign_e_date = request.form.get('endDate')
+
+    print(campaign_nam, campaign_vis, campaign_budg, campaign_desc, campaign_s_date, campaign_e_date)
+
+    spn = Sponsor.query.filter_by(sponsor_id=session.get('user_id')).first()
+    campaign = Campaign(
+        campaign_id=f"c_{sha_256(spn.sponsor_id + campaign_nam)}",
+        campaign_name=campaign_nam,
+        campaign_description=campaign_desc,
+        campaign_budget=int(campaign_budg),
+        campaign_visibility=campaign_vis,
+        campaign_start_date=date_pack(campaign_s_date),
+        campaign_end_date=date_pack(campaign_e_date),
+        sponsor=spn
+    )
+    
+    db.session.add(campaign)
+    db.session.commit()
+
+    flash("welcome to influencer search")
+
+    return redirect(url_for('home'))
+
+@app.route("/delete/campaign/<camp_id>",methods=["GET"])
+def delete_camp(camp_id):
+    campaign = Campaign.query.filter_by(campaign_id=camp_id).first()
+
+    if 'user_id' not in session or campaign.sponsor_id != session.get('user_id'):
+        flash("invalid user")
+        return redirect(url_for('home'))
+
+    db.session.delete(campaign)
+    db.session.commit()
+
+    flash("A campaign was deleted")
+    return redirect(url_for('home'))
+
+@app.route("/create/ad", methods=['POST'])
+def create_ad():
+    if 'user_id' not in session or session['user_id'][0] != 's':
+        return redirect(url_for('home'))
+    
+    camp_id = request.form.get('campId')
+    ad_nam = request.form.get('adName')
+    ad_budg = request.form.get('adBudg')
+    ad_req = request.form.get('adReq')
+
+    camp = Campaign.query.filter_by(campaign_id=camp_id).first()
+
+    if camp.campaign_visibility == "Public":
+        ad_stat = 'open'
+    else:
+        ad_stat = 'unsent'
+
+    advert = Advert(
+        ad_id = f'a_{sha_256(camp_id + ad_nam)}',
+        ad_name = ad_nam,
+        ad_budget = ad_budg,
+        ad_requirements = ad_req,
+        ad_accepted = ad_stat,
+
+        campaign = camp
+    )
+
+    db.session.add(advert)
+    db.session.commit()
+
+    return redirect(url_for('home'))  
+
+@app.route("/assign/ad",methods=['POST'])
+def assign_ad():
+    if 'user_id' not in session or session['user_id'][0] != 's':
+        return redirect(url_for('home'))
+    infId = request.form.get('adInf')
+    adId = request.form.get('adId')
+
+    inf = Influencer.query.filter_by(influencer_id = infId).first()
+    ad = Advert.query.filter_by(ad_id = adId).first()
+    ad.ad_accepted ='sent'
+
+    inf.adverts.append(ad)
+    db.session.commit()
+
+    return redirect(url_for('home'))
+
+@app.route("/accept/ad/<adId>",methods=['GET'])
+def accept_ad(adId):
+    if 'user_id' not in session or session['user_id'][0] != 'i':
+        return redirect(url_for('home'))
+    ad = Advert.query.filter_by(ad_id=adId).first()
+    if ad.influencer_id != session.get('user_id'):
+        flash("Invalid input")
+        return redirect(url_for('home'))
+    ad.ad_accepted = 'accepted'
+    db.session.commit()
+
+    return redirect(url_for('home'))
+
+@app.route("/take/ad/<adId>",methods=['GET'])
+def take_ad(adId):
+    if 'user_id' not in session or session['user_id'][0] != 'i':
+        return redirect(url_for('home'))
+    ad = Advert.query.filter_by(ad_id=adId).first()
+
+    inf = Influencer.query.filter_by(influencer_id=session.get("user_id")).first()
+
+    inf.adverts.append(ad)
+    ad.ad_accepted = 'accepted'
+    db.session.commit()
+
+    return redirect(url_for('home'))
+
+@app.route("/all/influencer")
+def all_inf():
+    if 'user_id' not in session or session['user_id'][0] != 's':
+        return redirect(url_for('home'))
+    
+    influencer = Influencer.query.filter_by().all()
+    
+    return render_template('all_inf.html',nav = True, infs = influencer)
+
+@app.route("/all/campaign")
+def all_camp():
+    if 'user_id' not in session or session['user_id'][0] != 'i':
+        return redirect(url_for('home'))
+    
+    campaign = Campaign.query.filter_by().all()
+
+    print(campaign)
+
+    return render_template('all_camp.html',nav = True, camps = campaign)
+
+
+@app.route("/statistics/influencer")
+def stat_inf():
+    if 'user_id' not in session or session['user_id'][0] != 'i':
+        return redirect(url_for('home'))
+    
+    influencer = Influencer.query.filter_by(influencer_id = session.get('user_id')).first()
+    # total ads accepted, total money
+
+    ads = influencer.adverts
+
+    ad_names = [ad.ad_name for ad in ads]
+    ad_rev = [int(ad.ad_budget) for ad in ads]
+
+    print(ad_names,ad_rev)
+    
+    
+    return render_template('statsinf.html' , nav = True , datas = ad_rev , lables = ad_names)
+
+@app.route("/statistics/sponsor")
+def stat_spn():
+    if 'user_id' not in session or session['user_id'][0] != 's':
+        return redirect(url_for('home'))
+    
+    spn = Sponsor.query.filter_by(sponsor_id = session.get('user_id')).first()
+
+    camp = spn.campaigns
+
+    c_names = [c.campaign_name for c in camp]
+    c_budget= [c.campaign_budget for c in camp]
+
+    return render_template('statsspn.html' , nav = True , datas = c_budget , lables = c_names)
--- backend/models.py
+++ backend/models.py
@@ -13,45 +13,51 @@ class Influencer(db.Model):
     influencer_niche = db.Column(db.String,nullable=False)
     influencer_platforms = db.Column(db.String,nullable=False)
     influencer_platform_reach = db.Column(db.String,nullable=True)
+    influencer_image = db.Column(db.LargeBinary, nullable=True)
+
+    adverts = db.relationship('Advert',backref='influencer')
 
 class Sponsor(db.Model):
     sponsor_id = db.Column(db.String,primary_key=True)
     sponsor_name = db.Column(db.String,nullable=False)
     sponsor_industry = db.Column(db.String,nullable=False)
     sponsor_budget = db.Column(db.Integer)
+    sponsor_image = db.Column(db.BLOB, nullable=True)
 
-# class Admin(db.Model):
-#     admin_name = db.Column(db.String)
+    campaigns = db.relationship('Campaign',backref='sponsor')
 
-# class Campaign(db.Model):
-#     campaign_id = db.Column(db.String,primary_key=True)
-#     campaign_name = db.Column(db.String,nullable=False)
-#     campaign_description = db.Column(db.String,nullable=False)
-#     campaign_budget = db.Column(db.Integer,nullable=False)
-#     campaign_visibility = db.Column(db.String,nullable=False)
-#     campaign_start_date = db.Column(db.Date,nullable=False)
-#     campaign_end_date = db.Column(db.Date,nullable=False)
+class Admin(db.Model):
+    admin_id = db.Column(db.String,primary_key=True)
+    admin_name = db.Column(db.String)
 
-# class Advert(db.Model):
-#     ad_id = db.Column(db.String,primary_key=True)
-#     campaign_id = db.Column(db.String,foreign_key=True)
-#     influencer_id = db.Column(db.String,foreign_key=True)
-#     ad_name = db.Column(db.String,nullable=False)
-#     ad_description = db.Column(db.String,nullable=False)
-#     ad_budget = db.Column(db.Integer,nullable=False)
-#     ad_visibility = db.Column(db.String,nullable=False)
-#     ad_start_date = db.Column(db.Date,nullable=False)
-#     ad_end_date = db.Column(db.Date,nullable=False)
-    # def __srt__():
+class Campaign(db.Model):
+    campaign_id = db.Column(db.String,primary_key=True)
+    campaign_name = db.Column(db.String,nullable=False)
+    campaign_description = db.Column(db.String,nullable=False)
+    campaign_budget = db.Column(db.Integer,nullable=False)
+    campaign_visibility = db.Column(db.String,nullable=False)
+    campaign_start_date = db.Column(db.Date,nullable=False)
+    campaign_end_date = db.Column(db.Date,nullable=False)
+
+    sponsor_id = db.Column(db.String,db.ForeignKey('sponsor.sponsor_id'))
+
+    adverts = db.relationship('Advert',backref='campaign')
 
 
-# class Advert(db.Model):
-#     ad_id = db.Column(db.String,primary_key=True)
-#     campaign_id = db.Column(db.String,foreign_key=True)
-#     influencer_id = db.Column(db.String,foreign_key=True)
-#     ad_name = db.Column(db.String,nullable=False)
-#     ad_description = db.Column(db.String,nullable=False)
-#     ad_budget = db.Column(db.Integer,nullable=False)
-#     ad_visibility = db.Column(db.String,nullable=False)
-#     ad_start_date = db.Column(db.Date,nullable=False)
-#     ad_end_date = db.Column(db.Date,nullable=False)
+class Advert(db.Model):
+    ad_id = db.Column(db.String,primary_key=True)
+    ad_name = db.Column(db.String,nullable=False)
+    ad_budget = db.Column(db.Integer,nullable=False)
+    ad_requirements = db.Column(db.String,nullable=False)
+    ad_accepted = db.Column(db.String,nullable=False)
+
+    influencer_id = db.Column(db.String,db.ForeignKey('influencer.influencer_id'))
+    campaign_id = db.Column(db.String,db.ForeignKey('campaign.campaign_id'))
+
+
+class Negotiation(db.Model):
+    neg_id = db.Column(db.String,primary_key=True)
+    neg_amount = db.Column(db.Integer,nullable=False)
+
+    neg_init = db.Column(db.String,nullable=False)
+    spn_id = db.Column(db.String,nullable=False)
